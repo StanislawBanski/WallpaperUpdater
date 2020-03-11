b08a04cae44b6fa97b706a94225f8440aebbd1d4
Stanisław Bański <staban95@gmail.com>
2020-03-11 16:33:13 +0100

    Add basic setup project for msi

---
+++ WallPaperWindowsService/WallPaperUpdater/CustomParameters.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Configuration.Install;
+
+namespace WallPaperUpdater
+{
+    public class CustomParameters
+    {
+        public const string WallpaperParameterKey = "WallpaperImageFilePath";
+        public const string LockScreenParameterKey = "LockScreenImageFilePath";
+
+        public string WallpaperParameterValue { get; } = null;
+        public string LockScreenParameterValue { get; } = null;
+
+        public CustomParameters(InstallContext installContext)
+        {
+            WallpaperParameterValue = installContext.Parameters[WallpaperParameterKey];
+            LockScreenParameterValue = installContext.Parameters[LockScreenParameterKey];
+        }
+
+        public CustomParameters(IDictionary savedState)
+        {
+            if (savedState != null)
+            {
+                if (savedState.Contains(WallpaperParameterKey) == true)
+                {
+                    WallpaperParameterValue = (string)savedState[WallpaperParameterKey];
+                }
+
+                if (savedState.Contains(LockScreenParameterKey) == true)
+                {
+                    LockScreenParameterValue = (string)savedState[LockScreenParameterKey];
+                }
+            } 
+        }
+    }
+}
--- WallPaperWindowsService/WallPaperUpdater/ProjectInstaller.cs
+++ WallPaperWindowsService/WallPaperUpdater/ProjectInstaller.cs
@@ -17,5 +17,41 @@ namespace WallPaperUpdater
             Context.Parameters["assemblypath"] += " " + "\"" + Context.Parameters["WallpaperImageFilePath"] + "\" " + "\"" + Context.Parameters["LockScreenImageFilePath"] + "\" ";
             base.OnBeforeInstall(savedState);
         }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            CustomParameters customParameters = new CustomParameters(Context);
+
+            SaveCustomParametersInStateSaverDictionary(stateSaver, customParameters);
+
+            base.Install(stateSaver);
+        }
+
+        private void SaveCustomParametersInStateSaverDictionary(IDictionary stateSaver, CustomParameters customParameters)
+        {
+            if (stateSaver.Contains(CustomParameters.WallpaperParameterKey) == true)
+            {
+                stateSaver[CustomParameters.WallpaperParameterKey] = customParameters.WallpaperParameterValue;
+            }              
+            else
+            {
+                stateSaver.Add(CustomParameters.WallpaperParameterKey, customParameters.WallpaperParameterValue);
+            }
+                
+
+            if (stateSaver.Contains(CustomParameters.LockScreenParameterKey) == true)
+            {
+                stateSaver[CustomParameters.LockScreenParameterKey] = customParameters.LockScreenParameterValue;
+            }            
+            else
+            {
+                stateSaver.Add(CustomParameters.LockScreenParameterKey, customParameters.LockScreenParameterValue);
+            }
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+        }
     }
 }
